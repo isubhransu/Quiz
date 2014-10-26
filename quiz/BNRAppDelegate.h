ae9d6c027af3fa6c5c5f91f3c67435d036d13390
Subhransu Mishra <isubhransu@gmail.com>
2014-10-25 23:52:58 -0700

    first commit

---
+++ quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  quiz
+//
+//  Created by Subhransu Mishra on 10/22/14.
+//  Copyright (c) 2014 Subhransu Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
