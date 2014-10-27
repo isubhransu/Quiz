35ba6bbeb4f7e5d04b8a5085b1e93ba0963b5504
Subhransu Mishra <isubhransu@gmail.com>
2014-10-26 17:58:38 -0700

    Setting up Quiz app View-Controller-Model
    Setting up and creating target BNRQuizViewControl(class) and its
    actions(method). Creating its model with question and answer array. And
    initializing in appdeligate  class.

---
+++ quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  quiz
+//
+//  Created by Subhransu Mishra on 10/26/14.
+//  Copyright (c) 2014 Subhransu Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
